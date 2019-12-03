49799bd750a468bf54e5a8e4115b41f90bb79a35
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-12-03 17:37:25 +0100

    Recreate Vault directory before extraction

--- restore-vault-data.sh
+++ restore-vault-data.sh
@@ -6,15 +6,17 @@ backup_file="${backup_dir}/vault_${backup_date}.tar.gz"
 vault_host="archilab-vault"
 nas_host="archilab-nas"
 
-ssh "${vault_host}" "mkdir -p ${backup_dir}"
+ssh "${vault_host}" "mkdir --parents ${backup_dir}"
 
 scp -3 "${nas_host}:${backup_file}" "${vault_host}:${backup_file}"
 
 ssh "${vault_host}" << EOF
     sudo systemctl stop vault vault-unseal
-    sudo rm -rf /var/lib/vault
+    sudo rm --recursive --force /var/lib/vault
+    sudo mkdir --parents /var/lib/vault
+    sudo chown --recursive vault:vault /var/lib/vault
     cd /
     sudo tar --extract --preserve-permissions --file="${backup_file}" --verbose
     sudo systemctl start vault vault-unseal
-    rm -rf ${backup_dir}
+    rm --recursive --force ${backup_dir}
 EOF
