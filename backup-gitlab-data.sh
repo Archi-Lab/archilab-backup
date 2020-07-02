48fc1af6c718d3dd801a8679933b2b813b7f9794
JannIntveen <JannIntveen@gmail.com>
2020-07-02 16:30:08 +0200

    fix gitlab backup script

--- backup-gitlab-data.sh
+++ backup-gitlab-data.sh
@@ -1,35 +1,46 @@
 #!/usr/bin/env bash
 
-current_date="$(date +%Y-%m-%d)"
+current_date="$(date +%Y_%m_%d_%H_%M_%S)"
 
-# Docker
-gitlab_container_name="gitlab_gitlab.1.n5qmwhqevd6yfm8lmeonht2n8"
+# Source Docker
+source_data_docker="/var/opt/gitlab/backups/dump_gitlab_backup.tar"
+source_configuration_docker="/etc/gitlab" 
 
 # Source
-source_data="\${HOME}/var/opt/gitlab/backups/dump_gitlab_backup.tar" # TODO Copy file out of docker container
-source_configuration="\${HOME}/etc/gitlab" 
+source_dir="/media/data/backups/${current_date}"
 
 # Target
-target_backup_dir="\${HOME}/archilab-gitlab"  
-target_data_backup_dir="${target_backup_dir}/data/${current_date}" 
-target_configuration_backup_dir="${target_backup_dir}/configuration/${current_date}" 
+target_dir="/homes/backup/archilab-gitlab"  
 
 # Hosts
-gitlab_host="archilab-gitlab"
+gitlab_host="gitlab" #"archilab-gitlab"
 nas_host="archilab-nas"
 
-# Create backup.tar
+# Create source dir
+echo "Create source dir: ${source_dir}"
+ssh "${gitlab_host}" "mkdir --parents ${source_dir}"
+
+# Docker
+gitlab_container_hash=$(ssh "${gitlab_host}" "docker service ps -f "name=gitlab_gitlab.1" gitlab_gitlab -q --no-trunc | head -n1")
+gitlab_container_name="gitlab_gitlab.1.${gitlab_container_hash}"
+echo "Docker Gitlab container name: ${gitlab_container_name}"
+
+echo "Start creating backup"
+# Create backup.tar and copy configuration files
 ssh "${gitlab_host}" <<EOF
-    sudo docker exec -t "${gitlab_container_name}" gitlab-backup create STRATEGY=copy BACKUP=dump
+    docker exec "${gitlab_container_name}" gitlab-backup create STRATEGY=copy BACKUP=dump
+    docker cp "${gitlab_container_name}":"${source_configuration_docker}" "${source_dir}"
+    docker cp "${gitlab_container_name}":"${source_data_docker}" "${source_dir}"
 EOF
 
-# Create target dirs
-ssh "${nas_host}" "mkdir --parents ${target_data_backup_dir}"
-ssh "${nas_host}" "mkdir --parents ${target_configuration_backup_dir}"
+echo "Saved backup to (Gitlab host): ${source_dir}"
 
 # Copy Backup files
-scp -3 "${gitlab_host}:${source_data}" "${nas_host}:${target_data_backup_dir}"
-scp -3 "${gitlab_host}:${source_configuration}" "${nas_host}:${target_configuration_backup_dir}"
+#scp -3 "${gitlab_host}:${source_dir}" "${nas_host}:${target_dir}"
 
-# Delete backup.tar
-ssh "${gitlab_host}" "rm --force ${source_data}"
+echo "Saved backup to (NAS): ${target_dir}"
+
+# Delete source dir
+ssh "${gitlab_host}" "rm -r ${source_dir}"
+
+echo "Deleted (Gitlab host): ${source_dir}"
