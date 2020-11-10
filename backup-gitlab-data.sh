fbdd70c329e14f222574e049d4da4efe88831ea7
JannIntveen <JannIntveen@gmail.com>
2020-11-10 12:50:42 +0100

    add feature which deletes old gitlab backups

--- backup-gitlab-data.sh
+++ backup-gitlab-data.sh
@@ -29,9 +29,9 @@ echo "Docker Gitlab container name: ${gitlab_container_name}"
 echo "Start creating backup"
 # Create backup.tar and copy configuration files
 ssh "${gitlab_host}" <<EOF
-    docker exec "${gitlab_container_name}" gitlab-backup create STRATEGY=copy BACKUP=dump
-    docker cp "${gitlab_container_name}":"${source_configuration_docker}" "${source_dir}"
-    docker cp "${gitlab_container_name}":"${source_data_docker}" "${source_dir}"
+   docker exec "${gitlab_container_name}" gitlab-backup create STRATEGY=copy BACKUP=dump
+   docker cp "${gitlab_container_name}":"${source_configuration_docker}" "${source_dir}"
+   docker cp "${gitlab_container_name}":"${source_data_docker}" "${source_dir}"
 EOF
 
 echo "Saved backup to (Gitlab host): ${source_dir}"
@@ -45,3 +45,29 @@ echo "Saved backup to (NAS): ${target_dir}"
 ssh "${gitlab_host}" "rm -r ${source_dir}"
 
 echo "Deleted (Gitlab host): ${source_dir}"
+
+# Delete old backups
+ssh "${nas_host}" << EOF
+   current_year=\$(date +%Y | sed 's/^0*//')
+   current_month=\$(date +%m | sed 's/^0*//')
+   current_day=\$(date +%d | sed 's/^0*//')
+
+   for file_path in "${target_dir}"/*;
+   do
+      file_name=\$(basename \$file_path)
+      file_year=\$(echo "\$file_name" | cut -d'_' -f1 | sed 's/^0*//')
+      file_month=\$(echo "\$file_name" | cut -d'_' -f2 | sed 's/^0*//')
+      file_day=\$(echo "\$file_name" | cut -d'_' -f3 | sed 's/^0*//')
+
+      year_diff=\$((current_year - file_year))
+      month_diff=\$((current_month - file_month))
+      day_diff=\$((current_day - file_day))
+
+      diff=\$((372 * year_diff + 31 * month_diff + day_diff))
+      if (( "\$diff" >= 7 ))
+      then
+         rm -r "\$file_path"
+         echo "Deleted backup \$file_name"
+      fi
+   done
+EOF
